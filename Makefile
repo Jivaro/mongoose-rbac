ca8039f8d55198bfb053d177c211e28e2d7e453c
Rasmus Kildevæld <admin@softshag.dk>
2015-03-16 23:53:38 +0100

    changed to promise style

--- Makefile
+++ Makefile
@@ -2,6 +2,6 @@ lint:
 	@./node_modules/.bin/jshint lib/
 
 test: lint
-	@NODE_ENV=test ./node_modules/.bin/mocha --require chai --reporter min
+	@NODE_ENV=test ./node_modules/.bin/mocha --require chai --reporter Spec
 
 .PHONY: lint test
--- test/rbac.test.js
+++ test/rbac.test.js
@@ -62,93 +62,95 @@ describe('roles and permissions:', function () {
   });
 
   it('should add a role to a model', function (next) {
-    henry.addRole('admin', function (err) {
-      expect(err).to.not.exist;
+    henry.addRole('admin').then(function () {
+      //expect(err).to.not.exist;
+
       expect(henry.roles).to.have.length(1);
       Role.findOne({ name: 'admin' }, function (err, role) {
         expect(henry.roles[0].equals(role._id)).to.be.ok;
+
         next();
       });
-    });
+    }, next);
   });
 
   it('should remove a role from a model', function (next) {
-    henry.addRole('admin', function (err) {
-      expect(err).to.not.exist;
-      henry.removeRole('admin', function (err) {
+    henry.addRole('admin').then(function (err) {
+
+      henry.removeRole('admin').then(function (err) {
         expect(err).to.not.exist;
         expect(henry.roles).to.be.empty;
         next();
       });
-    });
+    }, next);
   });
 
   it('should indicate whether a model has a given role', function (next) {
     expect(henry.roles).to.be.empty;
-    henry.hasRole('admin', function (err, hasAdminRole) {
-      expect(err).to.not.exist;
+    henry.hasRole('admin').then(function (hasAdminRole) {
       expect(hasAdminRole).to.equal(false);
-      henry.addRole('admin', function (err) {
-        expect(err).to.not.exist;
-        henry.hasRole('admin', function (err, hasAdminRole) {
-          expect(err).to.not.exist;
+      henry.addRole('admin').then(function () {
+
+        henry.hasRole('admin').then(function (hasAdminRole) {
+
           expect(hasAdminRole).to.equal(true);
           next();
         });
       });
-    });
+    }).catch(next);
   });
 
   it('should indicate whether a model has a given role when roles are populated', function (next) {
-    henry.addRole('admin', function (err) {
-      expect(err).to.not.exist;
-      henry.can('create', 'Post', function (err, can) {
-        expect(err).to.not.exist;
+    henry.addRole('admin').then(function () {
+
+      henry.can('create', 'Post').then(function (can) {
+        //expect(err).to.not.exist;
+        console.log('her')
         expect(can).to.equal(true);
-        henry.hasRole('admin', function (err, isAdmin) {
-          expect(err).to.not.exist;
+        henry.hasRole('admin').then(function (isAdmin) {
+          //expect(err).to.not.exist;
           expect(isAdmin).to.equal(true);
           next();
         });
       });
-    });
+    }).catch(next);
   });
 
   it('should indicate whether a model has a given permission', function (next) {
-    henry.addRole('readonly', function (err) {
-      expect(err).to.not.exist;
-      henry.can('read', 'Post', function (err, canReadPost) {
-        expect(err).to.not.exist;
+    henry.addRole('readonly').then(function (err) {
+      //expect(err).to.not.exist;
+      henry.can('read', 'Post').then(function (canReadPost) {
+        //expect(err).to.not.exist;
         expect(canReadPost).to.equal(true);
-        henry.can('create', 'Post', function (err, canCreatePost) {
-          expect(err).to.not.exist;
+        henry.can('create', 'Post').then(function (canCreatePost) {
+          //expect(err).to.not.exist;
           expect(canCreatePost).to.equal(false);
           next();
         });
       });
-    });
+    }).catch(next);
   });
 
   it('should indicate whether a model has all of a given set of permissions', function (next) {
-    henry.addRole('readonly', function (err) {
-      expect(err).to.not.exist;
-      henry.canAll([['read', 'Post'], ['read', 'Comment']], function (err, canRead) {
-        expect(err).to.not.exist;
+    henry.addRole('readonly').then(function (err) {
+      //expect(err).to.not.exist;
+      henry.canAll([['read', 'Post'], ['read', 'Comment']]).then(function (canRead) {
+
         expect(canRead).to.equal(true);
-        henry.canAll([['read', 'Post'], ['create', 'Post']], function (err, canReadAndCreate) {
-          expect(err).to.not.exist;
+        henry.canAll([['read', 'Post'], ['create', 'Post']]).then(function (canReadAndCreate) {
+
           expect(canReadAndCreate).to.equal(false);
           next();
         });
       });
-    });
+    }).catch(next);
   });
 
   it('should indicate whether a model has any of a given set of permissions', function (next) {
-    henry.addRole('readonly', function (err) {
-      expect(err).to.not.exist;
-      henry.canAny([['read', 'Post'], ['create', 'Post']], function (err, canReadOrCreate) {
-        expect(err).to.not.exist;
+    henry.addRole('readonly').then(function () {
+      //expect(err).to.not.exist;
+      henry.canAny([['read', 'Post'], ['create', 'Post']]).then(function (canReadOrCreate) {
+        //expect(err).to.not.exist;
         expect(canReadOrCreate).to.equal(true);
         next();
       });
@@ -156,11 +158,11 @@ describe('roles and permissions:', function () {
   });
 
   it('should be able to add multiple roles', function (next) {
-    henry.addRole('admin', function (err) {
-      expect(err).to.not.exist;
+    henry.addRole('admin').then(function (err) {
+      //expect(err).to.not.exist;
       expect(henry.roles).to.have.length(1);
-      henry.addRole('readonly', function (err) {
-        expect(err).to.not.exist;
+      henry.addRole('readonly').then(function (err) {
+        //expect(err).to.not.exist;
         expect(henry.roles).to.have.length(2);
         next();
       });
